eb357db179cb30527eef74063512c2b444709394
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2021-11-17 14:11:11 -0600

    Used some boolean logic

---
+++ BooleanAssingment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanAssingment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int Age = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false.");
+            string dui = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(dui);
+            Console.WriteLine("How many speeding tickets to you have?");
+            string speeding = Console.ReadLine();
+            int Tickets = Convert.ToInt32(speeding);
+
+            //Boolean Logic
+            bool ageBool = (Age > 15);
+            bool ticketsBool = (Tickets < 3);
+            bool NoDui = (DUI == false);
+            bool approved = (ageBool && ticketsBool && NoDui);
+            Console.WriteLine("Approved: " + approved);
+
+            Console.ReadLine();
+        }
+    }
+}
