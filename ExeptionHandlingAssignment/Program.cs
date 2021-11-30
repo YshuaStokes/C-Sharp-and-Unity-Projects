b1e43fb1b9fdd5ca4bf97c59bcfe1385d20d5493
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2021-11-30 16:07:00 -0600

    Exception Handling Assignment

---
+++ ExeptionHandlingAssignment/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeptionHandlingAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine("Pick a number.");
+            //    int numberOne = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Pick a second number.");
+            //    int numberTwo = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Dividing the two...");
+            //    int numberThree = numberOne / numberTwo;
+            //    Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            //    Console.ReadLine();
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    return;
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Please don't divide by 0");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.ReadLine();
+            //}
+
+            //Console.ReadLine();
+
+            List<int> list = new List<int> { 2, 4, 6, 8, 10 };
+
+            Console.WriteLine("Pick a number to divide all the numbers in a list.");
+            
+
+            try
+            {
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                foreach (int i in list)
+                {
+                    Console.WriteLine(i / numberOne);
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by 0");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Hooray, you understand math!!!");
+            Console.ReadLine();
+        }
+    }
+}
