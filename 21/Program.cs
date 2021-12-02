490a4f5530543ae8b63798c5791a335b0f729ca4
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2021-12-01 20:06:39 -0600

    MethodsSubmission

---
+++ 21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck = Shuffle(deck);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomeIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomeIndex]);
+                deck.Cards.RemoveAt(randomeIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+        }
+    }
+
+
+}
---
+++ MethodsSubmissionAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsSubmissionAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do math operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You chose the number " + userInput + "\n" + userInput + " + 5 is " + MyMethod1(userInput) + "\n" +
+                userInput + " * 6 is " + MyMethod2(userInput) +"\n" +
+                "7 - " + userInput + " is " + MyMethod3(userInput));
+            Console.ReadLine();
+            
+        }
+
+        public static int MyMethod1(int x)
+        {
+            return 5 + x;
+        }
+
+        public static int MyMethod2(int y)
+        {
+            return 6 * y;
+        }
+
+        public static int MyMethod3(int z)
+        {
+            return 7 - z;
+        }
+
+    }
+
+    
+}
