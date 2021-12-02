94d87a0d1ac17e90d9ff248b601ffb31a9e67f48
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2021-12-01 23:59:55 -0600

    main method

---
+++ MainMethodAssignment/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        public static int MyMethod(int x)
+        {
+            return 5 + x;
+        }
+
+        public static decimal MyMethod(double x)
+        {
+            int result = Convert.ToInt32(x * 9);
+            return result;
+        }
+
+        public static string MyMethod(string x)
+        {
+            string number = x;
+            int num = Convert.ToInt32(number);
+            
+            Console.WriteLine(num * 3);
+            return number;
+            
+        }
+       
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.MyMethod(2));
+
+            Console.WriteLine(Math.MyMethod(2.0));
+
+            Console.WriteLine(Math.MyMethod("4"));
+            
+            Console.ReadLine();
+        }
+    }
+}
