a9ebbc16e9f0a87659be8185504607f29ccd8e5e
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2021-12-03 12:21:07 -0600

    MethodClassSubmission

---
+++ MethodClassSubmissionAssignment/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    public class Math
+    {
+        public void MyMethod(int x, int y)
+        {
+            Console.WriteLine(x + 5);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math myMath = new Math();
+
+            myMath.MyMethod(x: 3, y: 4);
+
+            Console.ReadLine();
+        }
+    }
+}
