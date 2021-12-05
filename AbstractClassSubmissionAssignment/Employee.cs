a9c12b890fe281dfe3031c32cf7d894bff27833a
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2021-12-05 14:48:21 -0600

    AbstractClassSubmissionAssignment

---
+++ AbstractClassSubmissionAssignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmissionAssignment
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            
+
+        }
+    }
+}
